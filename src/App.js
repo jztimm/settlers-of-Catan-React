dee575dd24cfd56fd0c38a31cb169aaeaa5a86ae
Ethan Tola <ethanltola@gmail.com>
2020-09-30 20:38:38 -0400

    Refactored RandGenBtn to work and know only a len.
    The RandGenBtn doesn't need to be aware of the specific board being
    created. It only needs to know the length to generate.
    
    Signed-off-by: Ethan Tola <ethanltola@gmail.com>

--- src/App.js
+++ src/App.js
@@ -13,17 +13,17 @@ class App extends React.Component {
 
   state = {
     board: [
-      [0,1,2],
-      [3,4,5,6],
-      [7,8,9,10,11],
-      [12,13,14,15],
-      [16,17,18]
+      0,1,2,
+      3,4,5,6,
+      7,8,9,10,11,
+      12,13,14,15,
+      16,17,18
     ]
   }
 
   newBoard = (event) => {
     // let board = this.state.board
-    this.setState({ board: event})
+    this.setState({ board: event })
   }
 
   render() {
@@ -32,7 +32,7 @@ class App extends React.Component {
         <div>
           <Header />
           <Sidebar />
-          <span><RandGenBtn board={this.state.board} newBoard={this.newBoard} /></span>
+          <span><RandGenBtn len={this.board.length} newBoard={this.newBoard} /></span>
           <span><FaveBtn /></span>
           <MapContainer board={this.state.board}/>
         </div>
--- src/Components/RandGenBtn.js
+++ src/Components/RandGenBtn.js
@@ -5,15 +5,14 @@ import '../Style/Btn.css'
 class RandGenBtn extends React.Component {
 
    handleClick = () => {
-      for (let i = 0; i < this.props.board.length; i++) {
-         const randomBoard = this.props.board[Math.floor(Math.random() * this.props.board.length)]
-         this.props.newBoard(randomBoard)
-         console.log(randomBoard);
+       let newBoard = [];
+      for (let i = 0; i < this.props.len; i++) {
+        newBoard[i] = Math.floor(Math.random() * this.props.board.length)
       }
+      this.props.newBoard(newBoard);
    }
 
    render() {
-      // console.log(this.props.board)
       return (
          <div className="btn-rand">
             <button onClick={this.handleClick} className="btn">RandonGenBtn <FaRandom size="1.2em" className="RandBtn" /></button>
