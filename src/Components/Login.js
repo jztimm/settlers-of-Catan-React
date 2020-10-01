7b54d2d6c2f726d4f5f3f74ae839cea9c63ad875
jztimm <jztimm@gmail.com>
2020-10-01 18:32:54 -0400

    added sidebar to login

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,26 +1,30 @@
 import React from 'react'
+import Sidebar from './Sidebar'
 import "../App.css";
 
 export default class Login extends React.Component {
    render() {
       return (
+         <>
+         <Sidebar />
          <div className="Login">
          <form >
-         <input type="email" 
-                       className="form-control" 
-                       id="email" 
-                       aria-describedby="emailHelp" 
-                       placeholder="Enter email" 
-                       value=""
-                />
-      <input type="password" 
+            <input type="email" 
+                        className="form-control" 
+                        id="email" 
+                        aria-describedby="emailHelp" 
+                        placeholder="Enter email" 
+                        value=""
+               />
+            <input type="password" 
                         className="form-control" 
                         id="password" 
                         placeholder="Password"
                         value=""
-                    />
+               />
          </form>
          </div>
+         </>
       )
    }
 }
